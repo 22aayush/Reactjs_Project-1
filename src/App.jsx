e85e4b7a15567b525b0e2f970c75d342fdf542cf
aayush <goyalbindu578@gmail.com>
2024-06-21 01:07:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroSection from "./component/Hero.jsx";
+import Navigation from "./component/Navigation.jsx";
+import "./app.css";
+
+export default function App() {
+  return (
+    <>
+      <Navigation />
+      <HeroSection />
+    </>
+  )
+}
